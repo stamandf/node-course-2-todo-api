33d27e482a33409d8efd6de01a9f938e4bd8dcab
Florence St-Amand <stamandf@Florences-iMac.local>
2018-06-26 18:20:35 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,19 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+let _id = new ObjectID();
+const id = 123;
+
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
-},{text: 'Second test todo'
+},{
+  _id: new ObjectID(),
+  text: 'Second test todo'
 }];
 beforeEach((done) => {
   Todo.remove({}).then(() => {
@@ -66,3 +73,40 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo.text).toBe(todos[0].text);
+    })
+    .end(done);
+  });
+
+  it('should return 404 when id not found', (done) => {
+    request(app)
+    .get(`/todos/${_id.toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+  it('should return error message when id not found', (done) => {
+    request(app)
+    .get(`/todos/${_id.toHexString()}`)
+    .expect('Todo not found.', done);
+  });
+});
+  it('should return 404 for non-Object ids', (done) => {
+    request(app)
+    .get(`/todos/${id}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return a error message for non-Object ids', (done) => {
+    request(app)
+    .get(`/todos/${id}`)
+    .expect('Invalid ID.')
+    .end(done);
+  });
