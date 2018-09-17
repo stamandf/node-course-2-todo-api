9092d5a57d0a453ba4d6efe94326b0c9e2439793
Florence <stamandf@gmail.com>
2018-09-17 13:03:38 -0400

    Add error message for incorrect login.

--- server/server.js
+++ server/server.js
@@ -136,7 +136,7 @@ app.post('/users/login', (req,res) => {
     res.header('x-auth', token).send(user);
     });
   }).catch((e) => {
-    res.status(400).send(e);
+    res.status(400).send('Login or password incorrect.');
   });
 });
 module.exports = {app}
